6dd67dd893813849502278f9cec3665cc69e1142
Richard Amoo <amreechy@gmail.com>
2023-03-17 14:02:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
-#include <unistd.h> 
-#include <sys/syscall.h> 
-int main(void) 
-{ 
-    const char msg[] = "Hello, world!\n"; 
-    syscall(SYS_write, 1, msg, sizeof(msg)); 
-    return (1); 
+#include <unistd.h>
+#include <sys/syscall.h>
+int main(void)
+{
+    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n";
+    syscall(SYS_write, 1, msg, sizeof(msg));
+    return (1);
 }
