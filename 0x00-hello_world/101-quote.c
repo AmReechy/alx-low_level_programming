f0fe71e9b376346000111f4cd2b71621787afb70
Richard Amoo <amreechy@gmail.com>
2023-03-17 19:42:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,18 @@
+/*
+* Author: Richard Amoo
+* File: 101-quote.c
+*/
+
 #include <unistd.h>
-#include <sys/syscall.h>
+
+/**
+* main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+* Return: 1.
+*/
 int main(void)
 {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n";
-    syscall(SYS_write, 1, msg, sizeof(msg));
-    return (1);
+	write(2,
+			"and that piece of art is useful\" - Dora kopar, 2015-10-19\n",
+			59);
+	return (1);
 }
