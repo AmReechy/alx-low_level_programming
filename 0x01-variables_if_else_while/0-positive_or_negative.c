d426dda2d01009de119e9b4af658760b0f1721d2
Richard Amoo <amreechy@gmail.com>
2023-03-17 21:04:42 -0700

    first commit for the project

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+/**
+* main - Prints a random numbers (positive, 0 or negative)
+* Author: Richard Amoo
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Variable, If...else, while in C Programming (ALX)Project
