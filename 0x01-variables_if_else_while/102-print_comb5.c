d68c7b82dceca0b58d59f05ca1ffad34ca879927
Richard Amoo <amreechy@gmail.com>
2023-03-19 13:22:51 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+* main - prints all possible combinations of two two-digit numbers
+* Author: Richard Amoo
+* Return: 0
+*/
+int main(void)
+{int a = 0;
+int b;
+while (a < 99)
+	{b = a + 1;
+	while (b < 100)
+		{putchar('0' + a / 10);
+		putchar('0' + a % 10);
+		putchar(' ');
+		putchar('0' + b / 10);
+		putchar('0' + b % 10);
+		if (b == 99)
+		{putchar('\n');
+		break;
+		}
+		putchar(',');
+		putchar(' ');
+		b++;
+		}
+	a++;
+	}
+return (0);
+}
