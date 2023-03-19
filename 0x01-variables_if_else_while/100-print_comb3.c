c11f2b5056340bf120ad4e7bf080ec453ebd7ebc
Richard Amoo <amreechy@gmail.com>
2023-03-19 01:04:09 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main -  prints all possible different combinations of two digits
+* Author: Richard Amoo
+* Return: Always 0 (Success)
+*/i
+int main(void)
+{int num1 = 1;
+int num2;
+while (num1 < 8)
+	{
+	num2 = num1 + 1;
+	putchar(num1);
+	putchar(num2);
+	putchar(",");
+	putchar(" ");
+	num1++;
+	}
+	return (0);
+}
