452408df4a31a32b1b3b4dbfa7365f0c4ca059e0
Richard Amoo <amreechy@gmail.com>
2023-03-21 12:38:50 -0700

    2-print_alphabet_x10.c file added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Richard Amoo
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - prints 10 times the alphabet, in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	int letter = 0;
+
+	int n = 0;
+
+	while (n < 10)
+		{
+		while (letter <= 25)
+			{_putchar(letter + 'a');
+			letter++;
+			}
+		_putchar('\n');
+		n++;
+		}
+}
