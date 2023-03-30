a1905eabe5a48e0c8df0bed6c4d840f57c209129
Richard Amoo <amreechy@gmail.com>
2023-03-30 13:25:45 -0700

    103-infinite_add.c file committed

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,53 +1,69 @@
-#include "holberton.h"
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
 /**
- * infinite_add -  adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: result
- * @size_r: result lenght
- * Return: sum
- *
+ * reverse - reverses a string
+ * @s: string to reverse
+ * Return: pointer to the reversed string
+ */
+char *reverse(char *s)
+{
+	size_t len = strlen(s);
+	size_t i;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
+	}
+	return (s);
+}
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: The first positive number
+ * @n2: The second positive number
+ * @r: The buffer that the function will use to store the result
+ * @size_r: The buffer size
+ * Return: A pointer to the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-
 {
-	/* local variable declaration */
-	int i = 0, j = 0, k, l = 0, f, s, d = 0;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
 
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		j++;
-	if (i > j)
-		l = i;
-	else
-		l = j;
-	if (l + 1 > size_r)
+	char *temp = malloc(sizeof(char) * (len1 + len2 + 1));
+	int i, j, k, sum, carry = 0;
+
+	/* Perform addition */
+	for (i = len1 - 1, j = len2 - 1, k = 0;
+		i >= 0 || j >= 0 || carry;
+		i--, j--, k++)
+	{
+		sum = carry;
+		sum += (i >= 0) ? n1[i] - '0' : 0;
+		sum += (j >= 0) ? n2[j] - '0' : 0;
+		temp[k] = sum % 10 + '0';
+		carry = sum / 10;
+	}
+
+	/* if the result is empty, return 0 */
+	if (k == 0)
 		return (0);
-	r[l] = '\0';
-	for (k = l - 1 ; k >= 0 ; k--)
-	{
-		i--;
-		j--;
-		if (i >= 0)
-			f = n1[i] - '0';
-		else
-			f = 0;
-		if (j >= 0)
-			s = n2[j] - '0';
-		else
-			s = 0;
-		r[k] = (f + s + d) % 10 + '0';
-		d = (f + s + d) / 10;
-	}
-	if (d == 1)
-	{
-		r[l + 1] = '\0';
-		if (l + 2 > size_r)
-			return (0);
-		while (l-- >= 0)
-			r[l + 1] = r[l];
-		r[0] = d + '0';
-	}
-	return (r);
+
+	/* null terminate the result */
+	temp[k] = '\0';
+
+	/* if the result is too big, return 0 */
+	if ((int)strlen(temp) + 1 > size_r)
+		return (0);
+
+	r = strcpy(r, temp);
+	free(temp);
+
+	/* return the reversed result */
+	return (reverse(r));
 }
