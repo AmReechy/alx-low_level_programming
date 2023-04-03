3de50e8045676157b5d29b20f89186b73a21770a
Richard Amoo <amreechy@gmail.com>
2023-04-03 16:27:57 -0700

    3-strspn.c file added

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ void simple_print_buffer(char *buffer, unsigned int size);
 int main(void);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
