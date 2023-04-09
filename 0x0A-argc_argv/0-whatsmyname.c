12b8909a7fc61d6500a7f491223eb6ca9bdb9036
Richard Amoo <amreechy@gmail.com>
2023-04-09 07:13:15 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - function that prints its name; takes command line argument
+ * @argc: argument counts; number of argument supolied to main
+ * @argv: argument vector; pointer to array of pointers
+ * Return: void; no return value
+ */
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	char *pointr = *argv;
+	
+	while (*pointr)
+	{
+		_putchar(*pointr);
+		pointr++;
+	}
+	_putchar('\n');
+	return (0);
+}
