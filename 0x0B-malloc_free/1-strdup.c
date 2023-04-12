c445d03871768f3f25a460ba9b6a554009fb199d
Richard Amoo <amreechy@gmail.com>
2023-04-11 20:03:35 -0700

    1-strdup.c file added

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,8 +17,9 @@ char *_strdup(char *str)
 	unsigned int count;
 
 	n = strlen(str);
-	if (str == NULL)
+	if (str == NULL || strlen(str) == 0)
 	{
+		printf("failed to allocate memory\n");
 		return (NULL);
 	}
 	duplicate = (char *) malloc(n + 1 * sizeof(char));
