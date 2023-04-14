cf638f9a071be5aec19d34a2685d03f0993d22b4
Richard Amoo <amreechy@gmail.com>
2023-04-14 03:51:08 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	
+
 	newlen = new_size;
 	stop = new_size;
 	if (new_size > old_size)
