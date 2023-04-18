ba8e4d1425f9dacfaef641a6df0cbf6a06c7fbf0
Richard Amoo <amreechy@gmail.com>
2023-04-17 20:34:53 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOGGY
+#define DOGGY
+
+/**
+ * struct dog - INFO about a dog's name, age and owner
+ * @name: First member - name of the dog
+ * @age: Second member - age of the dog
+ * @owner: Third member - name of dog's owner
+ *
+ * Description: a Long description of the structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
