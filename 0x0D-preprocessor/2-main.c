55efda951b4cf684e60123ad7bb7a3f73871fd8c
Richard Amoo <amreechy@gmail.com>
2023-04-17 19:02:48 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - the function prints the name of the file it was compiled from
+ * Return: 0 for success
+ */
+
+int main()
+{
+	//trying out some comments with double forward slashes
+
+	printf("%s\n", __FILE__);
+
+	//putting in another comment line;
+	return (0);
+
+}
