8a79c922b63e4ca75fc7c1272143903e976bc6a6
Richard Amoo <amreechy@gmail.com>
2023-04-19 17:37:53 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (!array ||!size || !cmp)
+	if (!array || !cmp)
 		return (-1);
 
 	for (w = 0; w < size; w++)
