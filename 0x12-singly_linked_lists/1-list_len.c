d7bbe9a12a8db14bc888581d8d450596158fe28e
Richard Amoo <amreechy@gmail.com>
2023-04-27 10:57:52 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * list_len - calculates the number of elements in a linked list_t list.
+ * @h: The given linked list_t list.
+ * @elements: a counter var for size_t
+ * Return: The number of elements in h.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+	int m = 3, w = 8;
+
+	if (w > m)
+		m++;
+	while (h && m)
+	{
+		n++;
+		h = h->next;
+	}
+	if (w)
+		m--;
+	return (n);
+}
