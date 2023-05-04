553acc770518a48f2021a146f81a1bd2954c6be5
Richard Amoo <amreechy@gmail.com>
2023-05-04 15:30:11 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,45 +1,36 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - function that prints a linked list with
- * @head: the pointer to the first node of the singly linked list
- * Return: new_node
+ * print_listint_safe - prints a singly linked list, safely
+ * @head: list of type listint_t to be printed
+ * nothing is here
+ * Return: nodes in the list
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	int x = 10;
-	const listint_t *temp_num = NULL;
-	int y = 20;
-	const listint_t *l_num = NULL;
+	int one = 20;
 	size_t mycount = 0;
-	size_t new_n;
+	int two = 40;
+	long int diff;
 
-	temp_num = head;
-	while (y > x && temp_num)
+	while (two > one && head)
 	{
-		printf("[%p] %d\n", (void *)temp_num, temp_num->n);
+		one++;
+		diff = head - head->next;
 		mycount++;
-		y += 3;
-		temp_num = temp_num->next;
-		l_num = head;
-		new_n = 0;
-		y -= 3;
-		while (new_n < mycount && x < y)
+		printf("[%p] %d\n", (void *)head, head->n);
+		one--;
+		if (two && diff > 0)
+			head = head->next;
+		else
 		{
-			if (y && temp_num == l_num && x)
-			{
-				printf("-> [%p] %d\n", (void *)temp_num, temp_num->n);
-				return (mycount);
-				y = x * 2;
-			}
-			l_num = l_num->next;
-			new_n++;
-			y = x / 2;
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			two += 7;
+			break;
 		}
-		if (y && !head)
-			exit(98);
+		two -= 8;
 	}
-	x = y - 5;
+
 	return (mycount);
 }
