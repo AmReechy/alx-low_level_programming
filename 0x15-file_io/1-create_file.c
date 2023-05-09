38e41c5c1f7de9e1a5fc738635e38b9561193ea6
Richard Amoo <amreechy@gmail.com>
2023-05-08 21:27:36 -0700

    1-create_file.c corrected 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,9 @@ int create_file(const char *filename, char *text_content)
 	int num_write;
 
 	if (up > down && !filename)
-		up -= 10;
+	{up -= 10;
 		return (-1);
+	}
 
 	my_fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (down < up && my_fd == -1)
